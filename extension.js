388ea00fac54d29fa79f81d58ddc156308e93077
mc21fly <michalbkl@poczta.fm>
2022-03-29 11:04:19 +0200

    code refactor

--- extension.js
+++ extension.js
@@ -1,27 +1,59 @@
 const vscode = require('vscode');
-const CodeTracker = require('./src/CodeTracker');
+const window = vscode.window;
+const workspace = vscode.workspace;
 
 function activate(context) {
-	let codeTracker = new CodeTracker(vscode.window.activeTextEditor);
+	var activeTextEditor = window.activeTextEditor;
+	var configuration = workspace.getConfiguration('emaildev-utilities');
 
-	update();
+	init(configuration);
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('emaildev-utilities.helloWorld', async () => {
-
+			console.log(configuration);
 		})
 	);
 
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		codeTracker = new CodeTracker(editor);
-		update();
-	})
+	context.subscriptions.push(
+		vscode.commands.registerCommand('emaildev-utilities.toggleTables', async () => {
+			// Toggle tables highlight
+			configuration.update('isTablesEnabled', !configuration.get('isTablesEnabled'), true).then(() => {
+				//
+			});
+		})
+	);
 
-	function update() {
-		codeTracker.track('table', 'warning')
-		codeTracker.track('style', 'danger')
+	context.subscriptions.push(
+		vscode.commands.registerCommand('emaildev-utilities.toggleStyles', async () => {
+			// Toggle styles highlight
+			configuration.update('isStylesEnabled', !configuration.get('isStylesEnabled'), true).then(() => {
+				//
+			});
+		})
+	);
+
+	vscode.window.onDidChangeActiveTextEditor((editor) => {
+		// Activate when TextEditor change;
+		activeTextEditor = editor;
+
+		if (editor) update();
+	});
+
+	vscode.workspace.onDidChangeConfiguration(() => {
+		// Activate when configuration is changed;
+		configuration = workspace.getConfiguration('emaildev-utilities');
+
+		init(configuration);
+	});
+
+	function init(configuration) {
+		var trackTables = configuration.get('isTablesEnabled');
+		var trackStyles = configuration.get('isStylesEnabled');
+		var tablesTrackColor = configuration.get('tablesTrackColor');
+		var stylesTrackColor = configuration.get('stylesTrackColor');
 	}
 
+	function update() {}
 }
 
 exports.activate = activate;
