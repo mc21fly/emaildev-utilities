878e4d0710a873d7511d64fe92413cd9c1fe5371
mc21fly <michalbkl@poczta.fm>
2022-04-06 09:36:05 +0200

    refactor

--- src/Decoration.js
+++ src/Decoration.js
@@ -1,9 +1,10 @@
 const { window } = require("vscode");
+const { searchForTables } = require('./util');
 
 class Decoration {
-    constructor(configuration, type, range) {
+    constructor(configuration, type, textEditor) {
         this.type = window.createTextEditorDecorationType(configuration.get(`${type}Decoration`));
-        this.range = range;
+        this.range = searchForTables(textEditor);
         this.isActive = configuration.get(`is${type[0].toUpperCase() + type.substring(1)}Enabled`) && configuration.get('isExtensionEnabled');
     }
 
